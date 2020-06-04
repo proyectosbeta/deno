7937786cbe82b8e4573d3394be71d08ec2a6ff18
cmelgarejo <cmelgarejo.dev@gmail.com>
2020-06-04 13:11:47 -0400

    feat(ApiRest): removed unneeded imports Also added deno-workspace.yml for usage with denox, which that allows easy script execution a-la-npm Enjoy

--- ApiRest/app.ts
+++ ApiRest/app.ts
@@ -1,12 +1,5 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import router from "./router.ts";
-import {
-    getBooks,
-    getBook,
-    createBook,
-    updateBook,
-    deleteBook
-} from "./api/controller.ts";
 
 const env = Deno.env.toObject();
 const HOST = env.HOST || "127.0.0.1";
@@ -16,6 +9,5 @@ const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-
 console.log(`API is listening on ${HOST}:${PORT}...`);
 await app.listen(`${HOST}:${PORT}`);
